47e6525cbf767a7a20a360be36a23f8fe8ea59ee
fabricio.pereira <fabricio.pereira@ESTWKSNSI8204.anima.intranet>
2019-11-21 14:29:02 -0200

    add audio error quiz

--- src/view/telaError.java
+++ src/view/telaError.java
@@ -5,6 +5,9 @@
  */
 package view;
 
+import jaco.mp3.player.MP3Player;
+import java.io.File;
+
 /**
  *
  * @author fabricio.pereira
@@ -17,6 +20,8 @@ public class telaError extends javax.swing.JFrame {
     public telaError() {
         initComponents();
     }
+    public static final String Som ="C:\\Users\\fabricio.pereira\\Downloads\\Error.mp3";
+     MP3Player mp3player = new MP3Player(new File (Som));
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -31,6 +36,11 @@ public class telaError extends javax.swing.JFrame {
         buttonError = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowOpened(java.awt.event.WindowEvent evt) {
+                formWindowOpened(evt);
+            }
+        });
 
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/view/red.jpg"))); // NOI18N
 
@@ -56,6 +66,10 @@ public class telaError extends javax.swing.JFrame {
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
+
+    private void formWindowOpened(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowOpened
+mp3player.play(); // TODO add your handling code here:
+    }//GEN-LAST:event_formWindowOpened
 
     /**
      * @param args the command line arguments
